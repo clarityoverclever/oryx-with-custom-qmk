12fd71e5b3f0894b67eb1c4bb0190a3bf2e39c63
Keith Marshall <clarityoverclever@protonmail.com>
2026-01-23 14:08:32 -0800

    feat: add leader + git actions

--- GeEm3/keymap.c
+++ GeEm3/keymap.c
@@ -44,31 +44,31 @@ enum tap_dance_codes {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_voyager(
-    MT(MOD_LGUI, KC_ESCAPE),KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,       
-    CW_TOGG,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,        KC_BSLS,        
+    MT(MOD_LGUI, KC_ESCAPE),KC_1,           KC_2,           KC_3,           KC_4,           KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
+    CW_TOGG,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,        KC_BSLS,
     KC_LEFT_SHIFT,  KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                                           KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           MT(MOD_RSFT, KC_QUOTE),
-    KC_LEFT_CTRL,   MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_D,           KC_V,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_ENTER,       
-                                                    LT(1, KC_TAB),  KC_SPACE,                                       KC_SPACE,       TD(DANCE_0)
+    KC_LEFT_CTRL,   MT(MOD_LALT, KC_Z),KC_X,           KC_C,           KC_D,           KC_V,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_ENTER,
+                                                    LT(1, KC_TAB),  QK_LEAD,                                       KC_SPACE,       TD(DANCE_0)
   ),
   [1] = LAYOUT_voyager(
-    MT(MOD_LGUI, KC_ESCAPE),KC_F1,          KC_F2,          KC_F3,          KC_F4,          TD(DANCE_1),                                    KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         TD(DANCE_3),    
-    QK_LLCK,        KC_INSERT,      ST_MACRO_0,     ST_MACRO_1,     ST_MACRO_2,     KC_LPRN,                                        KC_RPRN,        KC_NO,          ST_MACRO_8,     KC_UP,          LSFT(KC_SCLN),  KC_NO,          
-    KC_LEFT_SHIFT,  KC_DELETE,      ST_MACRO_3,     ST_MACRO_4,     KC_HOME,        KC_LCBR,                                        KC_RCBR,        TD(DANCE_4),    KC_LEFT,        KC_DOWN,        KC_RIGHT,       TD(DANCE_5),    
-    KC_LEFT_CTRL,   KC_LEFT_ALT,    ST_MACRO_5,     ST_MACRO_6,     ST_MACRO_7,     TD(DANCE_2),                                    TD(DANCE_6),    ST_MACRO_9,     ST_MACRO_10,    ST_MACRO_11,    ST_MACRO_12,    KC_ENTER,       
+    MT(MOD_LGUI, KC_ESCAPE),KC_F1,          KC_F2,          KC_F3,          KC_F4,          TD(DANCE_1),                                    KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         TD(DANCE_3),
+    QK_LLCK,        KC_INSERT,      ST_MACRO_0,     ST_MACRO_1,     ST_MACRO_2,     KC_LPRN,                                        KC_RPRN,        KC_NO,          ST_MACRO_8,     KC_UP,          LSFT(KC_SCLN),  KC_NO,
+    KC_LEFT_SHIFT,  KC_DELETE,      ST_MACRO_3,     ST_MACRO_4,     KC_HOME,        KC_LCBR,                                        KC_RCBR,        TD(DANCE_4),    KC_LEFT,        KC_DOWN,        KC_RIGHT,       TD(DANCE_5),
+    KC_LEFT_CTRL,   KC_LEFT_ALT,    ST_MACRO_5,     ST_MACRO_6,     ST_MACRO_7,     TD(DANCE_2),                                    TD(DANCE_6),    ST_MACRO_9,     ST_MACRO_10,    ST_MACRO_11,    ST_MACRO_12,    KC_ENTER,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [2] = LAYOUT_voyager(
-    RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MODE_FORWARD,HSV_168_255_255,RGB_VAD,        RGB_VAI,                                        KC_MS_JIGGLER_TOGGLE,KC_NO,          KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,    KC_KP_PLUS,     
-    QK_LLCK,        KC_NO,          KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  LALT(LCTL(LSFT(KC_1))),                                KC_NO,          KC_NO,          KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_COMMA,    
-    KC_LEFT_SHIFT,  KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,LALT(LCTL(LSFT(KC_2))),                                KC_PSCR,        KC_NO,          KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_EQUAL,       
-    KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_KP_0,        KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_ENTER,       
+    RGB_TOG,        TOGGLE_LAYER_COLOR,RGB_MODE_FORWARD,HSV_168_255_255,RGB_VAD,        RGB_VAI,                                        KC_MS_JIGGLER_TOGGLE,KC_NO,          KC_KP_SLASH,    KC_KP_ASTERISK, KC_KP_MINUS,    KC_KP_PLUS,
+    QK_LLCK,        KC_NO,          KC_AUDIO_VOL_DOWN,KC_AUDIO_VOL_UP,KC_AUDIO_MUTE,  LALT(LCTL(LSFT(KC_1))),                                KC_NO,          KC_NO,          KC_KP_7,        KC_KP_8,        KC_KP_9,        KC_KP_COMMA,
+    KC_LEFT_SHIFT,  KC_MEDIA_PREV_TRACK,KC_MEDIA_NEXT_TRACK,KC_MEDIA_STOP,  KC_MEDIA_PLAY_PAUSE,LALT(LCTL(LSFT(KC_2))),                                KC_PSCR,        KC_NO,          KC_KP_4,        KC_KP_5,        KC_KP_6,        KC_EQUAL,
+    KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_KP_0,        KC_KP_1,        KC_KP_2,        KC_KP_3,        KC_ENTER,
                                                     KC_DOT,         KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
   [3] = LAYOUT_voyager(
     MT(MOD_LGUI, KC_ESCAPE),KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          NAVIGATOR_DEC_CPI,NAVIGATOR_INC_CPI,
-    QK_LLCK,        KC_NO,          KC_NO,          KC_MS_WH_UP,    KC_NO,          TOGGLE_SCROLL,                                  KC_NO,          KC_PGDN,        KC_MS_WH_UP,    KC_PAGE_UP,     KC_NO,          KC_NO,          
-    KC_LEFT_SHIFT,  KC_NO,          KC_MS_BTN2,     DRAG_SCROLL,    KC_MS_BTN1,     KC_MS_DBL_CLICK,                                KC_NO,          KC_MS_BTN1,     KC_MS_WH_DOWN,  KC_MS_BTN2,     KC_NO,          KC_NO,          
-    KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_NO,          KC_MS_WH_DOWN,  KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          
+    QK_LLCK,        KC_NO,          KC_NO,          KC_MS_WH_UP,    KC_NO,          TOGGLE_SCROLL,                                  KC_NO,          KC_PGDN,        KC_MS_WH_UP,    KC_PAGE_UP,     KC_NO,          KC_NO,
+    KC_LEFT_SHIFT,  KC_NO,          KC_MS_BTN2,     DRAG_SCROLL,    KC_MS_BTN1,     KC_MS_DBL_CLICK,                                KC_NO,          KC_MS_BTN1,     KC_MS_WH_DOWN,  KC_MS_BTN2,     KC_NO,          KC_NO,
+    KC_LEFT_CTRL,   KC_LEFT_ALT,    KC_NO,          KC_MS_WH_DOWN,  KC_NO,          KC_NO,                                          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,          KC_NO,
                                                     KC_TRANSPARENT, KC_TRANSPARENT,                                 KC_TRANSPARENT, KC_TRANSPARENT
   ),
 };
@@ -118,7 +118,7 @@ bool rgb_matrix_indicators_user(void) {
   if (rawhid_state.rgb_control) {
       return false;
   }
-  if (!keyboard_config.disable_layer_led) { 
+  if (!keyboard_config.disable_layer_led) {
     switch (biton32(layer_state)) {
       case 1:
         set_layer_color(1);
@@ -172,12 +172,12 @@ typedef struct {
 } tap;
 
 enum {
-    SINGLE_TAP = 1,      
-    SINGLE_HOLD,         
-    DOUBLE_TAP,          
-    DOUBLE_HOLD,         
-    DOUBLE_SINGLE_TAP,   
-    MORE_TAPS            
+    SINGLE_TAP = 1,
+    SINGLE_HOLD,
+    DOUBLE_TAP,
+    DOUBLE_HOLD,
+    DOUBLE_SINGLE_TAP,
+    MORE_TAPS
 };
 
 static tap dance_state[7];
@@ -451,7 +451,7 @@ tap_dance_action_t tap_dance_actions[] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
-  case QK_MODS ... QK_MODS_MAX: 
+  case QK_MODS ... QK_MODS_MAX:
     // Mouse keys with modifiers work inconsistently across operating systems, this makes sure that modifiers are always
     // applied to the mouse key that was pressed.
     if (IS_MOUSE_KEYCODE(QK_MODS_GET_BASIC_KEYCODE(keycode))) {
@@ -583,4 +583,72 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       return false;
   }
   return true;
+}
+
+static uint8_t last_rgb_mode;
+
+void leader_start_user(void) {
+    last_rgb_mode = rgb_matrix_get_mode();
+    rgb_matrix_mode_noanim(RGB_MATRIX_SOLID_COLOR)
+    rgb_matrix_set_color_all(0x00, 0xFF, 0xFF);
+}
+
+void leader_end_user(void) {
+    bool did_match = false;
+
+    // --- 3-Key Sequences ---
+    if (leader_sequence_three_keys(KC_G, KC_P, KC_D)) {
+        SEND_STRING("git pull\n");
+        did_match = true;
+    }
+    else if (leader_sequence_three_keys(KC_G, KC_P, KC_U)) {
+        SEND_STRING("git push\n");
+        did_match = true;
+    }
+
+    // --- 2-Key Sequences ---
+    else if (leader_sequence_two_keys(KC_Q, KC_Q)) {
+        // Sends Ctrl+C to kill any hung process, then clears terminal
+        SEND_STRING(SS_LCTL("c") "clear\n");
+        did_match = true;
+    }
+    else if (leader_sequence_two_keys(KC_G, KC_N)) {
+        // Prepare for a new branch name
+        SEND_STRING("git checkout -b ");
+        did_match = true;
+    }
+    else if (leader_sequence_two_keys(KC_G, KC_B)) {
+        // Prepare to switch branches
+        SEND_STRING("git checkout ");
+        did_match = true;
+    }
+    else if (leader_sequence_two_keys(KC_G, KC_S)) {
+        SEND_STRING("git status\n");
+        did_match = true;
+    }
+    else if (leader_sequence_two_keys(KC_G, KC_A)) {
+        SEND_STRING("git add .\n");
+        did_match = true;
+    }
+    else if (leader_sequence_two_keys(KC_G, KC_C)) {
+        // Prepare for the message
+        SEND_STRING("git commit -m \"");
+        did_match = true;
+    }
+
+    // --- Visual Feedback Logic ---
+    if (did_match) {
+        // Flash Green for success
+        rgb_matrix_set_color_all(0x00, 0xFF, 0x00);
+
+        // This creates a tiny "pause" so you actually see the green
+        // before the keyboard reverts to your normal layer colors.
+        wait_ms(150);
+    } else {
+        // Optional: Flash Red if you messed up the sequence
+        rgb_matrix_set_color_all(0xFF, 0x00, 0x00);
+        wait_ms(150);
+    }
+
+    rgb_matrix_mode_noanim(last_rgb_mode);
 }
