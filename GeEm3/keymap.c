18f361fae6f5266b7c3dc35c189abfe781ae60c0
Keith Marshall <clarityoverclever@protonmail.com>
2026-01-24 15:51:05 -0800

    fix: dance_1 -> dance_2

--- GeEm3/keymap.c
+++ GeEm3/keymap.c
@@ -50,8 +50,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     MT(MOD_LGUI, KC_ESCAPE),KC_1,           KC_2,           KC_3,           TD(DANCE_0),    KC_5,                                           KC_6,           KC_7,           KC_8,           KC_9,           KC_0,           KC_MINUS,
     CW_TOGG,        KC_Q,           KC_W,           KC_F,           KC_P,           KC_B,                                           KC_J,           KC_L,           KC_U,           KC_Y,           KC_SCLN,        KC_BSLS,
     KC_LEFT_SHIFT,  KC_A,           KC_R,           KC_S,           KC_T,           KC_G,                                           KC_M,           KC_N,           KC_E,           KC_I,           KC_O,           MT(MOD_RSFT, KC_QUOTE),
-    KC_LEFT_CTRL,   MT(MOD_LALT, KC_Z),KC_X,        KC_C,           KC_D,           KC_V,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         MT(MOD_RALT, KC_SLASH),KC_ENTER,
-                                                    LT(1, KC_TAB),  QK_LEAD,                                       KC_SPACE,       TD(DANCE_1)
+    KC_LEFT_CTRL,   MT(MOD_LALT, KC_Z),KC_X,        KC_C,           KC_D,           KC_V,                                           KC_K,           KC_H,           KC_COMMA,       KC_DOT,         TD(DANCE_1),KC_ENTER,
+                                                    LT(1, KC_TAB),  QK_LEAD,                                       KC_SPACE,       TD(DANCE_2)
   ),
   [1] = LAYOUT_voyager(
     MT(MOD_LGUI, KC_ESCAPE),KC_F1,          KC_F2,          KC_F3,          KC_F4,          TD(DANCE_3),                                    KC_F6,          KC_F7,          KC_F8,          KC_F9,          KC_F10,         TD(DANCE_7),
